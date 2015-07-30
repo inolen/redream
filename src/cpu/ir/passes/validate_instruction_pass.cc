0cbb175853f569fa84d70f124e06919cdf82e2d5
Anthony Pesch <inolen@gmail.com>
2015-07-30 11:21:35 -0700

    add OP_SELECT to ignore list for validate_instruction_pass

--- src/cpu/ir/passes/validate_instruction_pass.cc
+++ src/cpu/ir/passes/validate_instruction_pass.cc
@@ -17,8 +17,8 @@ void ValidateInstructionPass::ValidateInstr(Instr *instr) {
   // after constant propagation, there shouldn't be more than a single constant
   // argument for most instructions
   if (instr->op() != OP_STORE_CONTEXT && instr->op() != OP_BRANCH_COND &&
-      instr->arg0() && instr->arg0()->constant() && instr->arg1() &&
-      instr->arg1()->constant()) {
+      instr->op() != OP_SELECT && instr->arg0() && instr->arg0()->constant() &&
+      instr->arg1() && instr->arg1()->constant()) {
     LOG(FATAL) << "More than one constant argument detected for "
                << Opnames[instr->op()] << " instruction";
   }
