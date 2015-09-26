ff67901d431de11f627cbbfb5ec3adf910783ad2
Anthony Pesch <inolen@gmail.com>
2015-09-25 21:48:37 -0700

    added new sdiv test

--- test/asm/div1s.s
+++ test/asm/div1s.s
@@ -1,7 +1,7 @@
 test_div1s_16_ndividend:
   # REGISTER_IN r2 0x2710
   # REGISTER_IN r3 0x8012
-  # dividend is r3, divisor is r2
+  # r3 (16 bits) / r2 (16 bits) = r3 (16 bits)
   shll16 r2
   exts.w r3, r3
   xor r0, r0
@@ -36,7 +36,7 @@ test_div1s_16_ndividend:
 test_div1s_16_ndivisor:
   # REGISTER_IN r2 0xd8f0
   # REGISTER_IN r3 0x7fee
-  # dividend is r3, divisor is r2
+  # r3 (16 bits) / r2 (16 bits) = r3 (16 bits)
   shll16 r2
   exts.w r3, r3
   xor r0, r0
@@ -69,14 +69,12 @@ test_div1s_16_ndivisor:
   # REGISTER_OUT r3 0xfffffffd
 
 test_div1s_32_ndividend:
-  # REGISTER_IN r2 0x2710
+  # REGISTER_IN r2 0x00002710
+  # REGISTER_IN r1 0xffffffff
   # REGISTER_IN r3 0xf0000010
-  # dividend is r3, divisor is r2
-  mov r3, r0
-  rotcl r0
-  subc r1, r1
-  xor r0, r0
-  subc r0, r3
+  # r1:r3 (64 bits) / r2 (32 bits) = r3 (32 bits), remainder in r1
+  # t gets set to 1 here (r1 msb ^ r2 msb), and after 33 rotcl it should
+  # be the same at the end as well
   div0s r2, r1
   rotcl r3
   div1 r2, r1
@@ -143,20 +141,22 @@ test_div1s_32_ndividend:
   rotcl r3
   div1 r2, r1
   rotcl r3
-  addc r0, r3
+  movt r4
+  add r4, r3
   rts
   nop
+  # REGISTER_OUT r1 0xffffeac0
   # REGISTER_OUT r3 0xffff9725
+  # REGISTER_OUT r4 0x1
+
 
 test_div1s_32_ndivisor:
   # REGISTER_IN r2 0xffffd8f0
+  # REGISTER_IN r1 0x00000000
   # REGISTER_IN r3 0x0ffffff0
-  # dividend is r3, divisor is r2
-  mov r3, r0
-  rotcl r0
-  subc r1, r1
-  xor r0, r0
-  subc r0, r3
+  # r1:r3 (64 bits) / r2 (32 bits) = r3 (32 bits), remainder in r1
+  # t gets set to 1 here (r1 msb ^ r2 msb), and after 33 rotcl it should
+  # be the same at the end as well
   div0s r2, r1
   rotcl r3
   div1 r2, r1
@@ -223,7 +223,92 @@ test_div1s_32_ndivisor:
   rotcl r3
   div1 r2, r1
   rotcl r3
-  addc r0, r3
+  movt r4
+  add r4, r3
   rts
   nop
+  # REGISTER_OUT r1 0x00001540
   # REGISTER_OUT r3 0xffff9725
+  # REGISTER_OUT r4 0x1
+
+
+test_div1s_32:
+  # REGISTER_IN r2 0x00002710
+  # REGISTER_IN r1 0x0000000
+  # REGISTER_IN r3 0x0ffffff0
+  # r1:r3 (64 bits) / r2 (32 bits) = r3 (32 bits), remainder in r1
+  # t gets set to 0 here (r1 msb ^ r2 msb), and after 33 rotcl it should
+  # be the same at the end as well
+  div0s r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  div1 r2, r1
+  rotcl r3
+  movt r4
+  add r4, r3
+  rts
+  nop
+  # REGISTER_OUT r1 0x00001540
+  # REGISTER_OUT r3 0x000068db
+  # REGISTER_OUT r4 0x0
--- test/asm/div1u.s
+++ test/asm/div1u.s
@@ -1,5 +1,5 @@
 test_div1u_32_16:
-  # REGISTER_IN r0 0x2710
+  # REGISTER_IN r0 0x00002710
   # REGISTER_IN r1 0x0ffffff0
   # r1 (32 bits) / r0 (16 bits) = r1 (16 bits)
   shll16 r0
@@ -26,11 +26,13 @@ test_div1u_32_16:
   nop
   # REGISTER_OUT r1 0x68db
 
-test_div1u_64_32:
+test_div1u_32:
   # REGISTER_IN r0 0x00002710
-  # REGISTER_IN r1 0x00000001
-  # REGISTER_IN r2 0x2a05f200
-  # r1:r2 (64 bits) / r0 (32 bits) = r2 (32 bits)
+  # REGISTER_IN r1 0x00000000
+  # REGISTER_IN r2 0x2a05f207
+  # r1:r2 (64 bits) / r0 (32 bits) = r2 (32 bits), remainder in r1
+  # t gets cleared to 0 here, and after 33 rotcl it should
+  # be the same at the end as well
   div0u
   rotcl r2
   div1 r0, r1
@@ -97,6 +99,9 @@ test_div1u_64_32:
   rotcl r2
   div1 r0, r1
   rotcl r2
+  movt r4
   rts
   nop
-  # REGISTER_OUT r2 0x7a120
+  # REGISTER_OUT r1 0x00000a97
+  # REGISTER_OUT r2 0x00011367
+  # REGISTER_OUT r4 0x0
