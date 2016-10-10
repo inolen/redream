74e34692eb3e3e95e1e0df2258a050c8a97b2499
Anthony Pesch <inolen@gmail.com>
2016-10-09 19:37:17 -0700

    flash rom command parsing

--- src/hw/rom/boot.c
+++ src/hw/rom/boot.c
@@ -22,10 +22,9 @@ static void boot_rom_write(struct boot *boot, uint32_t addr, uint32_t data,
   LOG_FATAL("Can't write to boot rom");
 }
 
-static int boot_load(struct boot *boot, const char *path) {
+static int boot_load_rom(struct boot *boot, const char *path) {
   FILE *fp = fopen(path, "rb");
   if (!fp) {
-    LOG_WARNING("Failed to open boot at \"%s\"", path);
     return 0;
   }
 
@@ -34,18 +33,14 @@ static int boot_load(struct boot *boot, const char *path) {
   fseek(fp, 0, SEEK_SET);
 
   if (size != BIOS_SIZE) {
-    LOG_WARNING("BIOS size mismatch, is %d, expected %d", size, BIOS_SIZE);
+    LOG_WARNING("Boot rom size mismatch, is %d, expected %d", size, BIOS_SIZE);
     fclose(fp);
     return 0;
   }
 
   int n = (int)fread(boot->rom, sizeof(uint8_t), size, fp);
+  CHECK_EQ(n, size);
   fclose(fp);
-
-  if (n != size) {
-    LOG_WARNING("BIOS read failed");
-    return 0;
-  }
 
   return 1;
 }
@@ -53,7 +48,8 @@ static int boot_load(struct boot *boot, const char *path) {
 static bool boot_init(struct device *dev) {
   struct boot *boot = (struct boot *)dev;
 
-  if (!boot_load(boot, OPTION_bios)) {
+  if (!boot_load_rom(boot, OPTION_bios)) {
+    LOG_WARNING("Failed to load boot rom");
     return false;
   }
 
