1ff6f3e1c862ebd75ac511af0cd48f403ba909b3
Anthony Pesch <inolen@gmail.com>
2016-12-04 00:58:17 -0800

    added port / unit id to each registered maple device

--- src/hw/maple/maple.c
+++ src/hw/maple/maple.c
@@ -6,17 +6,18 @@
 #define MAPLE_NUM_PORTS 4
 #define MAPLE_MAX_UNITS 6
 
-struct maple_port {
-  struct maple_device *units[MAPLE_MAX_UNITS];
-};
-
 struct maple {
   struct device;
-  struct maple_port ports[MAPLE_NUM_PORTS];
+  struct maple_device *devices[MAPLE_NUM_PORTS][MAPLE_MAX_UNITS];
 };
 
-static bool maple_init(struct device *dev) {
-  return true;
+static void maple_register_device(struct maple *mp, struct maple_device *dev,
+                                  int port, int unit) {
+  CHECK(!mp->devices[port][unit],
+        "Device already registered for port %d, unit %d", port, unit);
+  mp->devices[port][unit] = dev;
+  dev->port = port;
+  dev->unit = unit;
 }
 
 static void maple_keydown(struct device *d, int device_index, enum keycode key,
@@ -26,15 +27,15 @@ static void maple_keydown(struct device *d, int device_index, enum keycode key,
   }
 
   struct maple *mp = (struct maple *)d;
-  struct maple_port *port = &mp->ports[device_index];
 
   /* create a controller if getting data from a new device */
-  if (!port->units[0]) {
-    port->units[0] = controller_create();
+  if (!mp->devices[device_index][0]) {
+    struct maple_device *controller = controller_create();
+    maple_register_device(mp, controller, device_index, 0);
   }
 
   for (int i = 0; i < MAPLE_MAX_UNITS; i++) {
-    struct maple_device *dev = port->units[i];
+    struct maple_device *dev = mp->devices[device_index][i];
 
     if (dev && dev->input) {
       dev->input(dev, key, value);
@@ -49,7 +50,7 @@ static void maple_keydown(struct device *d, int device_index, enum keycode key,
 
    7-6  5          4           3           2           1           0
    port main unit  sub-unit 5  sub-unit 4  sub-unit 3  sub-unit 2  sub-unit 1 */
-uint8_t maple_encode_addr(int port, int unit) {
+static uint8_t maple_encode_addr(int port, int unit) {
   CHECK_LT(port, MAPLE_NUM_PORTS);
   CHECK_LT(unit, MAPLE_MAX_UNITS);
 
@@ -63,14 +64,15 @@ uint8_t maple_encode_addr(int port, int unit) {
   return addr;
 }
 
-void maple_decode_addr(uint32_t addr, int *port, int *unit) {
+static void maple_decode_addr(uint32_t addr, int *port, int *unit) {
   *port = addr >> 6;
-  *unit = -1;
+  CHECK_LT(*port, MAPLE_NUM_PORTS);
 
+  /* prioritize the main unit, else return the first matching sub-unit */
+  *unit = -1;
   if (addr & (1 << (MAPLE_MAX_UNITS - 1))) {
     *unit = 0;
   } else {
-    /* return the first matching sub-unit */
     for (int i = 1; i < MAPLE_MAX_UNITS; i++) {
       if (addr & (1 << (i - 1))) {
         *unit = i;
@@ -78,8 +80,7 @@ void maple_decode_addr(uint32_t addr, int *port, int *unit) {
       }
     }
   }
-
-  CHECK_NE(*unit, -1);
+  CHECK(*unit >= 0 && *unit < MAPLE_MAX_UNITS);
 }
 
 int maple_handle_command(struct maple *mp, struct maple_frame *frame,
@@ -87,8 +88,7 @@ int maple_handle_command(struct maple *mp, struct maple_frame *frame,
   int p, u;
   maple_decode_addr(frame->header.recv_addr, &p, &u);
 
-  struct maple_port *port = &mp->ports[p];
-  struct maple_device *dev = port->units[u];
+  struct maple_device *dev = mp->devices[p][u];
 
   if (!dev) {
     return 0;
@@ -105,7 +105,7 @@ int maple_handle_command(struct maple *mp, struct maple_frame *frame,
      in addition to bit 5 */
   if (u == 0) {
     for (int i = 1; i < MAPLE_MAX_UNITS; i++) {
-      struct maple_device *sub = port->units[i];
+      struct maple_device *sub = mp->devices[p][i];
       if (sub) {
         res->header.send_addr |= (1 << (i - 1));
       }
@@ -115,12 +115,14 @@ int maple_handle_command(struct maple *mp, struct maple_frame *frame,
   return 1;
 }
 
+static bool maple_init(struct device *dev) {
+  return true;
+}
+
 void maple_destroy(struct maple *mp) {
   for (int i = 0; i < MAPLE_NUM_PORTS; i++) {
-    struct maple_port *port = &mp->ports[i];
-
     for (int j = 0; j < MAPLE_MAX_UNITS; j++) {
-      struct maple_device *dev = port->units[j];
+      struct maple_device *dev = mp->devices[i][j];
 
       if (dev && dev->destroy) {
         dev->destroy(dev);
@@ -138,8 +140,13 @@ struct maple *maple_create(struct dreamcast *dc) {
   mp->window_if = dc_create_window_interface(NULL, &maple_keydown);
 
   /* add one controller and vmu by default */
-  mp->ports[0].units[0] = controller_create();
-  mp->ports[0].units[1] = vmu_create();
+  {
+    struct maple_device *controller = controller_create();
+    maple_register_device(mp, controller, 0, 0);
+
+    struct maple_device *vmu = vmu_create();
+    maple_register_device(mp, vmu, 0, 1);
+  }
 
   return mp;
 }
