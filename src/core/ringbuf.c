c7629dc3efc48dbd185b0eaa9249bbb8315e6d88
Anthony Pesch <inolen@gmail.com>
2016-12-11 11:46:14 -0800

    added single producer / single consumer ring buffer added atomic load / fetch / store / exchange macros to work around msvc not supporting stdatomic.h added sleep shim for windows

---
+++ src/core/ringbuf.c
@@ -0,0 +1,57 @@
+#include "core/ringbuf.h"
+#include "core/assert.h"
+#include "sys/atomic.h"
+
+struct ringbuf {
+  int capacity;
+  void *data;
+  struct re_atomic_long read_offset;
+  struct re_atomic_long write_offset;
+};
+
+void ringbuf_advance_write_ptr(struct ringbuf *rb, int n) {
+  ATOMIC_FETCH_ADD(rb->write_offset, n);
+}
+
+void *ringbuf_write_ptr(struct ringbuf *rb) {
+  int write_offset = ATOMIC_LOAD(rb->write_offset);
+  return rb->data + (write_offset % rb->capacity);
+}
+
+void ringbuf_advance_read_ptr(struct ringbuf *rb, int n) {
+  ATOMIC_FETCH_ADD(rb->write_offset, n);
+}
+
+void *ringbuf_read_ptr(struct ringbuf *rb) {
+  int read_offset = ATOMIC_LOAD(rb->read_offset);
+  return rb->data + (read_offset % rb->capacity);
+}
+
+int ringbuf_remaining(struct ringbuf *rb) {
+  return ringbuf_capacity(rb) - ringbuf_available(rb);
+}
+
+int ringbuf_available(struct ringbuf *rb) {
+  int read = ATOMIC_LOAD(rb->read_offset);
+  int write = ATOMIC_LOAD(rb->write_offset);
+  int available = write - read;
+  CHECK(available >= 0 && available <= rb->capacity);
+  return available;
+}
+
+int ringbuf_capacity(struct ringbuf *rb) {
+  return rb->capacity;
+}
+
+void ringbuf_destroy(struct ringbuf *rb) {
+  free(rb);
+}
+
+struct ringbuf *ringbuf_create(int capacity) {
+  struct ringbuf *rb = calloc(1, sizeof(struct ringbuf));
+
+  rb->capacity = capacity;
+  rb->data = malloc(capacity);
+
+  return rb;
+}
---
+++ src/sys/atomic.h
@@ -0,0 +1,42 @@
+#ifndef SYS_ATOMIC_H
+#define SYS_ATOMIC_H
+
+#include "core/config.h"
+
+#ifdef HAVE_STDATOMIC_H
+
+#include <stdatomic.h>
+
+struct re_atomic_int {
+    atomic_int x;
+};
+
+struct re_atomic_long {
+    atomic_long x;
+};
+
+#define ATOMIC_LOAD(a) atomic_load(&a.x)
+#define ATOMIC_FETCH_ADD(a, value) atomic_fetch_add(&a.x, value)
+#define ATOMIC_STORE(a, value) atomic_store(&a.x, value)
+#define ATOMIC_EXCHANGE(a, value) atomic_exchange(&a.x, value)
+
+#else
+
+#include <atomic>
+
+struct re_atomic_int {
+    std::atomic<int> x;
+};
+
+struct re_atomic_long {
+    std::atomic<long> x;
+};
+
+#define ATOMIC_LOAD(a) (a.x.load())
+#define ATOMIC_FETCH_ADD(a, value) (a.x.fetch_add(value))
+#define ATOMIC_STORE(a, value) (a.x.store(value))
+#define ATOMIC_EXCHANGE(a, value) (a.x.exchange(value))
+
+#endif
+
+#endif
