0341443091d867a2a58e025935fe5d72e077b9af
Anthony Pesch <inolen@gmail.com>
2016-12-17 14:27:54 -0800

    reenabled interval tree tests

--- src/core/interval_tree.h
+++ src/core/interval_tree.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 #include "core/rb_tree.h"
 
-#define INTERVAL_NODE(n) ((struct interval_node *)n)
+#define INTERVAL_NODE(n) (container_of_safe((n), struct interval_node, rb))
 
 typedef uintptr_t interval_type_t;
 
 struct interval_node {
-  struct rb_node base;
+  struct rb_node rb;
   interval_type_t low;
   interval_type_t high;
   interval_type_t max;
@@ -26,6 +26,18 @@ struct interval_tree_it {
 void interval_tree_insert(struct rb_tree *t, struct interval_node *n);
 void interval_tree_remove(struct rb_tree *t, struct interval_node *n);
 void interval_tree_clear(struct rb_tree *t);
+
+interval_type_t interval_tree_max(struct rb_tree *t);
+int interval_tree_size(struct rb_tree *t);
+int interval_tree_height(struct rb_tree *t);
+
+struct interval_node *interval_tree_min_interval(struct interval_node *n,
+                                                 interval_type_t low,
+                                                 interval_type_t high);
+struct interval_node *interval_tree_next_interval(struct interval_node *n,
+                                                  interval_type_t low,
+                                                  interval_type_t high);
+
 struct interval_node *interval_tree_find(struct rb_tree *t, interval_type_t low,
                                          interval_type_t high);
 
