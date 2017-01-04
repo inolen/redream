7e09bcd99991d9ea48076e4d4626fe8650108945
Anthony Pesch <inolen@gmail.com>
2017-01-04 00:45:47 -0800

    restructure main emulator loop to be driven by the amount of audio available in the aica ring buffer (#1)
    made audio backend discard an incoming amount of aica frames proportionate to any silence that was previously written out to keep audio time domain in sync with emulator

--- src/audio/audio_backend.h
+++ src/audio/audio_backend.h
@@ -7,6 +7,7 @@ struct audio_backend;
 struct audio_backend *audio_create(struct aica *aica);
 void audio_destroy(struct audio_backend *audio);
 
+int audio_buffer_low(struct audio_backend *audio);
 void audio_pump_events(struct audio_backend *audio);
 
 #endif
