15eab031fe5a2a5f60d659d3fc02e75d65502b27
Anthony Pesch <inolen@gmail.com>
2017-06-13 21:35:47 -0400

    added disc_meta structure which represents the meta information contained in the ip.bin

--- src/core/string.h
+++ src/core/string.h
@@ -22,7 +22,8 @@ size_t strnlen(const char *s, size_t max_len);
 char *strnstr(const char *s1, const char *s2, size_t n);
 #endif
 
-void strncpy_spaces(char *dst, const char *str, int size);
+void strncpy_pad_spaces(char *dst, const char *str, int size);
+void strncpy_trim_spaces(char *dst, const char *str, int size);
 
 int strnrep(char *dst, size_t dst_size, const char *token, size_t token_len,
             const char *value, size_t value_len);
