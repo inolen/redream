90f610a7d2cecf46349272fb05eb518d8d52253b
Anthony Pesch <inolen@gmail.com>
2017-06-17 18:56:40 -0400

    remove unused FindClangTidy script

--- cmake/FindClangTidy.cmake
+++
@@ -1,5 +0,0 @@
-find_program(CLANG_TIDY_EXECUTABLE NAMES clang-tidy clang-tidy-3.6 clang-tidy-3.7)
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(clang_tidy
-  DEFAULT_MSG
-  CLANG_TIDY_EXECUTABLE)
