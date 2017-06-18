97af83c33d57dd27a9f96734518cd04b6abe44f4
Anthony Pesch <inolen@gmail.com>
2017-06-18 17:59:57 -0400

    fix retest bit rot

--- src/host/null_host.c
+++ src/host/null_host.c
@@ -9,20 +9,22 @@ void audio_push(struct host *base, const int16_t *data, int num_frames) {}
 /*
  * video
  */
-void video_gl_make_current(struct host *host, gl_context_t ctx) {}
+void video_unbind_context(struct host *base) {}
 
-void video_gl_destroy_context(struct host *base, gl_context_t ctx) {}
+void video_bind_context(struct host *base, struct render_backend *r) {}
 
-gl_context_t video_gl_create_context_from(struct host *base,
-                                          gl_context_t from) {
+void video_destroy_renderer(struct host *base, struct render_backend *r) {}
+
+struct render_backend *video_create_renderer_from(struct host *base,
+                                                  struct render_backend *from) {
   return NULL;
 }
 
-gl_context_t video_gl_create_context(struct host *base) {
+struct render_backend *video_create_renderer(struct host *base) {
   return NULL;
 }
 
-int video_gl_supports_multiple_contexts(struct host *base) {
+int video_supports_multiple_contexts(struct host *base) {
   return 0;
 }
 
