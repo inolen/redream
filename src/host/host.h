fb489f71128a6f6d1f11d9e0c8a1b7f5704a7958
Anthony Pesch <inolen@gmail.com>
2017-10-08 00:34:24 -0400

    fix null host bitrot

--- src/host/host.h
+++ src/host/host.h
@@ -1,9 +1,15 @@
 #ifndef HOST_H
 #define HOST_H
 
+#include <stdint.h>
+
 struct host;
 
 /* audio */
 void audio_push(struct host *host, const int16_t *data, int frames);
+
+/* video */
+
+/* input */
 
 #endif
--- src/host/null_host.c
+++ src/host/null_host.c
@@ -9,44 +9,7 @@ void audio_push(struct host *base, const int16_t *data, int num_frames) {}
 /*
  * video
  */
-void video_unbind_context(struct host *base) {}
-
-void video_bind_context(struct host *base, struct render_backend *r) {}
-
-void video_destroy_renderer(struct host *base, struct render_backend *r) {}
-
-struct render_backend *video_create_renderer_from(struct host *base,
-                                                  struct render_backend *from) {
-  return NULL;
-}
-
-struct render_backend *video_create_renderer(struct host *base) {
-  return NULL;
-}
-
-int video_supports_multiple_contexts(struct host *base) {
-  return 0;
-}
-
-void video_set_fullscreen(struct host *base, int fullscreen) {}
-
-int video_is_fullscreen(struct host *base) {
-  return 0;
-}
-
-int video_can_fullscreen(struct host *base) {
-  return 0;
-}
-
-int video_height(struct host *base) {
-  return 0;
-}
-
-int video_width(struct host *base) {
-  return 0;
-}
 
 /*
  * input
  */
-void input_poll(struct host *base) {}
